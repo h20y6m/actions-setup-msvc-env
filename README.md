8566781a05f0e8a0399adabcbb799e311a6c641c
Yukimasa Morimi <h20y6m@yahoo.co.jp>
2026-04-29 17:04:01 +0900

    various

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # actions-setup-msvc-env
 GitHub Action: setup MSVC environment variables
+
+## Useage
+
+### Inputs
+
+```yaml
+- uses: h20y6m/actions-setup-msvc-env@v1
+  with:
+    # Target architecture.
+    # Optional. Default is $RUNNER_ARCH.
+    arch:
+
+    # Windows SDK version.
+    # Optional. Default is latest installed version.
+    sdk:
+
+    # VC++ compiler toolset version.
+    # Optional. Default is latest installed version.
+    toolset:
+
+    # Visual Studio version.
+    # Optional. Default is latest installed version.
+    vsversion:
+```
--- setup-msvc-env.ps1
+++ setup-msvc-env.ps1
@@ -1,122 +1,161 @@
 # Setup MSVC Environment Variables
 
 param(
-    [string]$Arch = "x64",
+    [string]$Arch = "",
     [string]$Sdk = "",
-    [string]$ToolSet = "",
-    [string]$VsVersion = "latest"
+    [string]$Toolset = "",
+    [string]$VsVersion = ""
 )
 
-$arch = $Arch
-if (-not $arch) {
-    $arch = "x64"
+if (-not $Arch) {
+    if ($env:RUNNER_ARCH) {
+        $Arch = $env:RUNNER_ARCH.ToLower()
+    }
+    else {
+        $Arch = $env:PROCESSOR_ARCHITECTURE.ToLower()
+    }
 }
 
-$vsversion = $VsVersion
-if (-not $vsversion) {
-    $vsversion = "latest"
+switch ($VsVersion) {
+    "2017" { $VsVersion = "[15.0,16.0)" }
+    "2019" { $VsVersion = "[16.0,17.0)" }
+    "2022" { $VsVersion = "[17.0,18.0)" }
+    "2026" { $VsVersion = "[18.0,19.0)" }
 }
-switch ($vsversion) {
-    "2017" { $vsversion = "[15.0,16.0)" }
-    "2019" { $vsversion = "[16.0,17.0)" }
-    "2022" { $vsversion = "[17.0,18.0)" }
-    "2026" { $vsversion = "[18.0,19.0)" }
-}
-if ($vsversion -match "^(\d+)(\.\d)?$") {
+if ($VsVersion -match "^(\d+)(\.\d)?$") {
     $max = ([int]$matches[1] + 1)
-    $vsversion = "[$vsversion,$max)"
+    $VsVersion = "[$VsVersion,$max)"
 }
-# Write-Host "::notice::vsversion = `"$vsversion`""
+# Write-Host "::notice::VsVersion = `"$VsVersion`""
 
 $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
 # Write-Host "::notice::vswhere = [$vswhere]"
 
 if (-not (Test-Path $vswhere)) {
-    Write-Error "vswhere.exe not found"
+    Write-Host "##[error]vswhere.exe not found"
     exit 1
 }
 
-if ("$vsversion" -eq "latest") {
-    $vspath = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+if ($VsVersion) {
+    $vspath = & $vswhere -version $VsVersion -products * -requires Microsoft.Component.MSBuild -property installationPath
 }
 else {
-    $vspath = & $vswhere -version $vsversion -products * -requires Microsoft.Component.MSBuild -property installationPath
+    $vspath = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
 }
 if ($LASTEXITCODE) {
-    Write-Error "vswhere failed: $LASTEXITCODE"
+    Write-Host "##[error]vswhere failed: $LASTEXITCODE"
     if ($vspath) {
         $vspath | Where-Object { $_ -match "^Error" } | ForEach-Object { Write-Error $_ }
     }
     exit 1
 }
-# Write-Host "::notice::vspath = [$vspath]"
 
 if (-not $vspath) {
-    Write-Error "Visual Studio not found"
+    Write-Host "##[error]Visual Studio not found"
     exit 1
 }
 
 $vcvars = Join-Path $vspath "VC\Auxiliary\Build\vcvarsall.bat"
-# Write-Host "::notice::vcvars = [$vcvars]"
 
 if (-not (Test-Path $vcvars)) {
-    Write-Error "vcvarsall.bat not found"
+    Write-Host "##[error]vcvarsall.bat not found"
     exit 1
 }
 
 Write-Host "Found with vswhere: $vcvars"
 
-# Run vcvarsall.bat and capture environment variables
-$outBefore = New-TemporaryFile
-$outVcvars = New-TemporaryFile
-$outAfter  = New-TemporaryFile
-
+# vcvarsall.bat options
 $optVcvars = ""
 if ($Sdk) {
     $optVcvars += " $Sdk"
 }
-if ($ToolSet) {
-    $optVcvars += " -vcvars_ver=$ToolSet"
+if ($Toolset) {
+    switch ($Toolset.ToLower()) {
+        "vs2017" { $Toolset = "v141" }
+        "vs2019" { $Toolset = "v142" }
+        "vs2022" { $Toolset = "v143" }
+        "vs2026" { $Toolset = "v145" }
+    }
+    if ($Toolset.StartsWith("v")) {
+        $defaultTxtPath = Join-Path $vspath "VC\Auxiliary\Build\Microsoft.VCToolsVersion.$Toolset.default.txt"
+        if (Test-Path $defaultTxtPath) {
+            $Toolset = Get-Content -Path $defaultTxtPath -TotalCount 1
+        }
+        else {
+            # VS2026 hasn't Microsoft.VCToolsVersion.v145.default.txt
+            $vPath = Join-Path $vspath "VC\Auxiliary\Build\$Toolset"
+            if (Test-Path $vPath) {
+                $toolsVersions = Get-ChildItem -Path $vPath -File -Filter "Microsoft.VCToolsVersion.VC.*.txt" | ForEach-Object {
+                    Get-Content -Path $_ -TotalCount 1
+                }
+                $latest = $null
+                foreach ($toolsVersion in $toolsVersions) {
+                    try {
+                        $version = [version]$toolsVersion
+                        if (!$latest -or $latest -lt $version) {
+                            $latest = $version
+                            $Toolset = $toolsVersion
+                        }
+                    }
+                    catch {}
+                }
+            }
+        }
+    }
+    $optVcvars += " -vcvars_ver=$Toolset"
 }
+# Write-Host "::notice::optVcvars = [$optVcvars]"
 
-cmd /c "SET >`"$outBefore`" && `"$vcvars`" $arch $optVcvars >`"$outVcvars`" && SET >`"$outAfter`""
-if ($LASTEXITCODE) {
-    Write-Error "vcvarsall.bat failed: $LASTEXITCODE"
-    exit 1
+# Run vcvarsall.bat and capture environment variables
+$tmpBefore = New-TemporaryFile
+$tmpAfter = New-TemporaryFile
+try {
+    $output = & cmd /c "SET >`"$tmpBefore`" && `"$vcvars`" $Arch $optVcvars && SET >`"$tmpAfter`""
+    $exitcode = $LASTEXITCODE
+    $outBefore = Get-Content -Path $tmpBefore
+    $outAfter = Get-Content -Path $tmpAfter
+}
+finally {
+    if (Test-Path $tmpBefore) { Remove-Item -Path $tmpBefore -Force -ErrorAction SilentlyContinue }
+    if (Test-Path $tmpAfter) { Remove-Item -Path $tmpAfter -Force -ErrorAction SilentlyContinue }
 }
 
 # Check vcvarsall.bat error
-$errVcvars = Select-String -Path $outVcvars -Pattern "^\[ERROR" | Select-Object -ExpandProperty Line
+if ($exitcode) {
+    Write-Host "vcvarsall.bat failed: $exitcode"
+    exit 1
+}
+$errVcvars = $output | Where-Object { $_ -match "^\[ERROR" }
 if ($errVcvars) {
-    Write-Error "vcvarsall.bat failed"
+    Write-Host "##[error]vcvarsall.bat failed"
     foreach ($line in $errVcvars) {
-        Write-Error $line
+        Write-Host "##[error]$line"
     }
     exit 1
 }
 
 # Save environment variables before running vcvarsall.bat
 $before = @{}
-Get-Content $outBefore | ForEach-Object {
-    if ($_ -match "^(.*?)=(.*)$") {
+foreach ($line in $outBefore) {
+    if ($line -match "^([^=]+?)=(.*)$") {
+        $key = $matches[1].ToLower()
         $before[$matches[1]] = $matches[2]
     }
 }
 
-Write-Host "::group::Environment variables"
-
 # Set the added or modified environment variables
-Get-Content $outAfter | ForEach-Object {
-    if ($_ -match "^(.*?)=(.*)$") {
-        $key = $matches[1]
-        $val = $matches[2]
-        if (-not $before.ContainsKey($key) -or $before[$key] -ne $val) {
-            Write-Host "Setting $key"
-            "$key=$val" >> $env:GITHUB_ENV
+Write-Host "::group::Environment variables"
+foreach ($line in $outAfter) {
+    if ($line -match "^([^=]+?)=(.*)$") {
+        $name = $matches[1]
+        $value = $matches[2]
+        $key = $name.ToLower()
+        if (-not $before.ContainsKey($key) -or $before[$key] -ne $value) {
+            Write-Host "Setting $name"
+            "$name=$value" >> $env:GITHUB_ENV
         }
     }
 }
-
 Write-Host "::endgroup::"
 
 Write-Host "Configured Developer Command Prompt"
